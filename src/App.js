1bc4f8fde7af1e8b87adde5c728b8bfc85ecd188
N <237604@NTTDATA.COM>
2023-01-19 12:29:54 +0530

    little lemon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Globalstyle } from './globalstyle';
+import { Routes, Route } from 'react-router';
+import Hero from './components/Hero/Hero';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
+import Feature from './components/Features';
+import Footer from './components/Footer';
+import Form from './components/Form';
+import Success from './components/Success';
+import Aos from 'aos';
 
 function App() {
+  const [cartItem, setCartItem] = useState(0);
+  Aos.init({ duration: 1500, delay: 500 });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Globalstyle />
+      <Routes>
+        <Route
+          path='/'
+          exact
+          element={[
+            <Hero cartItem={cartItem} setCartItem={setCartItem} />,
+            <Products
+              heading='Our Top Picks'
+              id='fast-foods'
+              data={productData}
+              cartItem={cartItem}
+              setCartItem={setCartItem}
+            />,
+            <Feature />,
+            <Products
+              heading='Our Top Deserts'
+              id='deserts'
+              data={productDataTwo}
+              cartItem={cartItem}
+              setCartItem={setCartItem}
+            />,
+            <Footer />,
+          ]}
+        ></Route>
+        <Route path='/booking' element={<Form />}></Route>
+        <Route path='/success' element={<Success />}></Route>
+      </Routes>
+    </>
   );
 }
 
